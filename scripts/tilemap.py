76ed0a9475e91fdb26951c11e9147bb96586940f
Juan <jsfigueroa1805@gmail.com>
2026-01-21 14:17:33 -0500

    def tile_around en scripts/tilemap.py añadida

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,3 +1,5 @@
+NEIGHBOR_OFFSET = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (0, 0), (-1, 1), (0, 1), (1, 1)]
+
 class Tilemap:
     def __init__(self, game, tile_size = 16):
         self.game  = game
@@ -7,7 +9,16 @@ class Tilemap:
 
         for i in range(10):
             self.tilemap[str(3 + i) + ';10'] = {'type': 'grass', 'variant': 1, 'pos': (3 + i, 10)}
-            self.tilemap[';10' + str(5 + i)] = {'type': 'grass', 'variant': 1, 'pos': (10, 5 + i)}
+            self.tilemap['10;' + str(5 + i)] = {'type': 'stone', 'variant': 1, 'pos': (10, 5 + i)}
+
+    def tile_around(self, pos):
+        tiles = []
+        tile_loc = (int(pos[0] // self.tile_zise), int(pos[1] // self.tile_zise))
+        for offset in NEIGHBOR_OFFSET:
+            check_loc = str(tile_loc[0] + offset[0]) + ';' + str(tile_loc[1] + offset[1])
+            if check_loc in self.tilemap:
+                tiles.append(self.tile_map[check_loc])
+        return tiles
 
     def render(self, surf):
         for tile in self.offgrid_tiles:
@@ -17,10 +28,11 @@ class Tilemap:
             tile = self.tilemap[loc]
             surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_zise['pos'][1] * self.tile_zise))
 
-
+ 
         
 
 
 
 
 
+ 
