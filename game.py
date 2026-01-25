39df8c3bc7e0c839f122c14e35f1c323b8d7675b
Juan <jsfigueroa1805@gmail.com>
2026-01-25 12:26:41 -0500

    Camara de seguimiento agregada

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
 
 
+
 class Game:
     def __init__(self): 
         pg.init()
@@ -28,15 +29,21 @@ class Game:
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         
         self.tilemap = Tilemap(self, tile_size = 16)
+
+        self.scroll = [0, 0]
         
     def run(self):      
         while True: 
             self.display.fill((14, 219, 248))
 
-            self.tilemap.render(self.display)
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset = render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
+            self.player.render(self.display, offset = render_scroll)
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
