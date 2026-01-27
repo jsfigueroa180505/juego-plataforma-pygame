87213ed7b7d20d7d02db2976385644fbdd13c6de
Juan Figueroa <jsfigueroa1805@gmail.com>
2026-01-27 11:39:47 -0500

    editor de niveles primeros conseptos

--- editor.py
+++ editor.py
@@ -5,75 +5,93 @@ import sys
 from scripts.utils import load_images
 from scripts.tilemap import Tilemap
 
-
+RENDER_SCALE = 2.0
 
 class Editor:
     def __init__(self): 
         pg.init()
 
-        pg.display.set_caption('Ninja Game') 
+        pg.display.set_caption('Editor') 
         self.screen = pg.display.set_mode((640, 480)) 
         self.display = pg.Surface((320, 240))
 
         self.clock = pg.time.Clock() 
-
-        self.movement = [False, False]
         
         self.assets = {
             'decor': load_images('tiles/decor'),
             'grass': load_images('tiles/grass'),
             'large_decore': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png'),
-            'background': load_image('background.png'),
-            'clouds': load_images('clouds'),
-            'player/idle': Animation(load_images('entities/player/idle'), img_dur = 6),
-            'player/run': Animation(load_images('entities/player/run'), img_dur = 4),
-            'player/jump': Animation(load_images('entities/player/jump')),
-            'player/slide': Animation(load_images('entities/player/slide')),
-            'player/wall_slide': Animation(load_images('entities/player/wall_slide')),
         }
 
-        self.clouds = Clouds(self.assets['clouds'], count = 16)
-        
-        self.player = Player(self, (50, 50), (8, 15))
+        self.movement = [False, False, False, False]
         
         self.tilemap = Tilemap(self, tile_size = 16)
 
         self.scroll = [0, 0]
+
+        self.tile_list = list(self.assets)
+        self.tile_group = 0
+        self.tile_variant = 0
+
+        self.clicking = False
+        self.right_clicking = False
+        self.shift = False
         
     def run(self):      
         while True: 
-            self.display.blit(self.assets['background'], (0, 0))
+            self.display.fill((0, 0, 0))
 
-            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
-            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
-            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+            current_tile_img = self.assets[self.tile_list[self.tile_group]][self.tile_variant].copy()
+            current_tile_img.set_alpha(100)
 
-            self.clouds.update()
-            self.clouds.render(self.display, offset = render_scroll)
-
-            self.tilemap.render(self.display, offset = render_scroll)
-
-            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0)) 
-            self.player.render(self.display, offset = render_scroll)
+            self.display.blit(current_tile_img, (5, 5))
 
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
+
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        self.clicking = True
+                    if event.button == 3:
+                        self.right_clicking = True
+                    if self.shift:
+                        if event.button == 4:
+                            self.tile_variant = (self.tile_variant - 1) % len(self.assets[self.tile_list[self.tile_group]])
+                        if event.button == 5:
+                            self.tile_variant = (self.tile_variant + 1) % len(self.assets[self.tile_list[self.tile_group]])
+                    else:
+                        if event.button == 4:
+                            self.tile_group = (self.tile_group - 1) % len(self.tile_list)
+                            self.tile_variant = 0
+                        if event.button == 5:
+                            self.tile_group = (self.tile_group + 1) % len(self.tile_list)
+                            self.tile_variant = 0
+                            
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_LEFT:
                         self.movement[0] = True
                     if event.key == pg.K_RIGHT:
                         self.movement[1] = True
                     if event.key == pg.K_UP:
-                        self.player.velocity[1] = -3
+                        self.movement[2] = True
+                    if event.key == pg.K_DOWN:
+                        self.movement[3] = True  
+                    if event.key == pg.K_LSHIFT:
+                        self.shift = True 
                 if event.type == pg.KEYUP:
                     if event.key == pg.K_LEFT:
                         self.movement[0] = False
                     if event.key == pg.K_RIGHT:
                         self.movement[1] = False
+                    if event.key == pg.K_UP:
+                        self.movement[2] = False
+                    if event.key == pg.K_DOWN:
+                        self.movement[3] = False 
+                    if event.key == pg.K_LSHIFT:
+                        self.shift = False 
             
             self.screen.blit(pg.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pg.display.update() 
