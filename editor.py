5d81c14186b0eec856405a21e448c20aacb75e82
Juan Figueroa <jsfigueroa1805@gmail.com>
2026-01-30 10:46:05 -0500

    funcion para eliminar objetos en el editor añadida

--- editor.py
+++ editor.py
@@ -55,6 +55,10 @@ class Editor:
 
             if self.clicking:
                 self.tilemap.tilemap[str(tile_pos[0]) + ';' + str(tile_pos[1])] = {'type': self.tile_list[self.tile_group], 'variant': self.tile_variant, 'pos': tile_pos}
+            if self.right_clicking:
+                tile_loc =str(tile_pos[0]) + ';' + str(tile_pos[1])
+                if tile_loc in self.tilemap.tilemap:
+                    del self.tilemap.tilemap[tile_loc]
 
             self.display.blit(current_tile_img, (5, 5))
 
