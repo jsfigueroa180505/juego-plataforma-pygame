1615c7840069453df01c4c502bac9b1e6aba644a
Juan Figueroa <jsfigueroa1805@gmail.com>
2026-02-10 16:54:18 -0500

    cambios de estructuracion realizados

--- scripts/particle.py
+++ scripts/particle.py
@@ -1,12 +1,12 @@
 class Particle:
-    def __init__(self, game, p_type, pos, velocity = [0, 0], frame = 0):
+    def __init__(self, game, p_type, pos, velocity=[0, 0], frame=0):
         self.game = game
-        self.p_type = p_type
+        self.type = p_type
         self.pos = list(pos)
         self.velocity = list(velocity)
         self.animation = self.game.assets['particle/' + p_type].copy()
         self.animation.frame = frame
-        
+    
     def update(self):
         kill = False
         if self.animation.done:
@@ -14,12 +14,12 @@ class Particle:
         
         self.pos[0] += self.velocity[0]
         self.pos[1] += self.velocity[1]
-
+        
         self.animation.update()
-
+        
         return kill
     
-    def render(self, surf, offset = (0, 0)):
+    def render(self, surf, offset=(0, 0)):
         img = self.animation.img()
         surf.blit(img, (self.pos[0] - offset[0] - img.get_width() // 2, self.pos[1] - offset[1] - img.get_height() // 2))
-        
+    
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,22 +1,22 @@
-import pygame as pg
 import os
 
+import pygame
 
-BASE_IMAGE_PATH = 'data/images/'
+BASE_IMG_PATH = 'data/images/'
 
 def load_image(path):
-    img = pg.image.load(BASE_IMAGE_PATH + path).convert()
+    img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
-    return img  
+    return img
 
 def load_images(path):
     images = []
-    for img_name in sorted(os.listdir(BASE_IMAGE_PATH + path)):
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
         images.append(load_image(path + '/' + img_name))
     return images
 
 class Animation:
-    def __init__(self, images, img_dur = 5, loop = True):
+    def __init__(self, images, img_dur=5, loop=True):
         self.images = images
         self.loop = loop
         self.img_duration = img_dur
@@ -29,10 +29,10 @@ class Animation:
     def update(self):
         if self.loop:
             self.frame = (self.frame + 1) % (self.img_duration * len(self.images))
-        else: 
+        else:
             self.frame = min(self.frame + 1, self.img_duration * len(self.images) - 1)
             if self.frame >= self.img_duration * len(self.images) - 1:
                 self.done = True
-                
+    
     def img(self):
         return self.images[int(self.frame / self.img_duration)]
