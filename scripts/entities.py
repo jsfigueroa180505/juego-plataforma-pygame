e4d3c0f146ec23f36dfd474c1ee30333afcaf4de
Juan Figueroa <jsfigueroa1805@gmail.com>
2026-02-11 15:11:03 -0500

    funcion saltar para jugador creada

--- scripts/entities.py
+++ scripts/entities.py
@@ -70,17 +70,25 @@ class Player(PhysicsEntity):
     def __init__(self, game, pos, size):
         super().__init__(game, 'player', pos, size)
         self.air_time = 0
-    
+        self.jumps = 2
+
     def update(self, tilemap, movement=(0, 0)):
         super().update(tilemap, movement=movement)
         
         self.air_time += 1
         if self.collisions['down']:
             self.air_time = 0
-            
+            self.jumps = 2
+
         if self.air_time > 4:
             self.set_action('jump')
         elif movement[0] != 0:
             self.set_action('run')
         else:
             self.set_action('idle')
+
+    def jump(self):
+        if self.jumps:
+            self.velocity[1] = -3
+            self.jumps -= 1
+            self.air_time = 5
